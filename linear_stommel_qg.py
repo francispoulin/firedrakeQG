ef764ef9b50b43462428084c230561d67b1b5e63
fpoulin <fpoulin@uwaterloo.ca>
2016-11-26 07:28:26 -0500

    adding code to solve for the linear Stommel problem

---
+++ linear_stommel_qg.py
@@ -0,0 +1,54 @@
+from firedrake import *
+import numpy as np
+#import sys
+
+# Geometry
+Lx   = 1.                                            # Zonal length
+Ly   = 1.                                            # Meridonal length
+n0   = 50                                            # Spatial resolution
+mesh = RectangleMesh(n0, n0, Lx, Ly,  reorder=None) 
+
+# Function and Vector Spaces
+Vcg  = FunctionSpace(mesh,"CG",3)                    # CG elements for Streamfunction
+Vdg  = FunctionSpace(mesh,"DG",1)                    # DG elements for Potential Vorticity (PV)
+Vcdg = VectorFunctionSpace(mesh,"DG",2)              # DG elements for velocity
+
+# Boundary Conditions
+def boundary(x, on_boundary):
+	return on_boundary
+bc = DirichletBC(Vcg, 0.0, boundary)
+
+# Physical parameters
+beta = Constant(1.0)                                 # Beta parameter
+F    = Constant(1.0)                                 # Burger number
+r    = Constant(0.2)                                 # Bottom drag
+tau  = Constant(0.001)                               # Wind Forcing
+
+# Test Functions
+phi, p, v = TestFunction(Vcg), TestFunction(Vdg), TestFunction(Vcdg)
+
+# Trial Functions
+psi, q, u = TrialFunction(Vcg), TrialFunction(Vdg), TrialFunction(Vcdg)
+
+# Solution Functions
+psi_soln, u_soln = Function(Vcg), Function(Vcdg)
+q0_soln, qn_soln = Function(Vdg), Function(Vcdg)
+
+# Find Linear Solution: First Streamfunction then Potential Vorticity
+Fwinds = Function(Vcg).interpolate(Expression("-tau*cos(pi*(x[1]-0.5))", tau=tau))
+a = -( r*(inner(nabla_grad(psi), nabla_grad(phi)) + F*psi*phi) - beta*phi*psi.dx(0) )*dx
+L = Fwinds*phi*dx
+#solve(a==L, psi_soln, bc)
+
+# Set up Elliptic inverter
+psi_problem = LinearVariationalProblem(a,L,psi_soln, bcs=bc)
+psi_solver = LinearVariationalSolver(psi_problem,
+                                     solver_parameters={
+                                         'ksp_type':'cg',
+                                         'pc_type':'sor'
+                                      })
+
+#q0_soln = project(div(grad(psi_soln)) - F*psi_soln, DG)
+
+#plot(psi_soln)
+#interactive()
