a2fd1b064491e23035cf4af0618f103baa7315c6
Francis J. Poulin <fpoulin@uwaterloo.ca>
2016-11-28 23:22:52 -0500

    Add files via upload

--- linear_stommel_qg.py
+++ linear_stommel_qg.py
@@ -1,6 +1,6 @@
 from firedrake import *
 import numpy as np
-#import sys
+import matplotlib.pyplot as plt 
 
 # Geometry
 Lx   = 1.                                            # Zonal length
@@ -13,19 +13,16 @@ Vcg  = FunctionSpace(mesh,"CG",3)                    # CG elements for Streamfun
 Vdg  = FunctionSpace(mesh,"DG",1)                    # DG elements for Potential Vorticity (PV)
 Vcdg = VectorFunctionSpace(mesh,"DG",2)              # DG elements for velocity
 
-psi0 = Function(Vcg, name='Streamfunction') 
+#psi0 = Function(Vcg, name='Streamfunction') 
 
 # Boundary Conditions
-#def boundary(x, on_boundary):
-#	return on_boundary
-#bc = DirichletBC(Vcg, 0.0, boundary)
 bc = DirichletBC(Vcg, 0.0, (1, 2, 3, 4))
 
 # Physical parameters
-beta = Constant(1.0)                                 # Beta parameter
-F    = Constant(1.0)                                 # Burger number
-r    = Constant(0.2)                                 # Bottom drag
-tau  = Constant(0.001)                               # Wind Forcing
+beta = Constant("1.0")                                 # Beta parameter
+F    = Constant("1.0")                                 # Burger number
+r    = Constant("0.2")                                 # Bottom drag
+tau  = Constant("0.001")                               # Wind Forcing
 
 # Test Functions
 phi, p, v = TestFunction(Vcg), TestFunction(Vdg), TestFunction(Vcdg)
@@ -34,27 +31,38 @@ phi, p, v = TestFunction(Vcg), TestFunction(Vdg), TestFunction(Vcdg)
 psi, q, u = TrialFunction(Vcg), TrialFunction(Vdg), TrialFunction(Vcdg)
 
 # Solution Functions
-psi_soln, u_soln = Function(Vcg), Function(Vcdg)
+psi_soln, u_soln = Function(Vcg, name="Streamfunction"), Function(Vcdg)
 q0_soln, qn_soln = Function(Vdg), Function(Vcdg)
 
-# Find Linear Solution: First Streamfunction then Potential Vorticity
+# Define Winds and Weak Form
 Fwinds = Function(Vcg).interpolate(Expression("-tau*cos(pi*(x[1]-0.5))", tau=tau))
-a = -( r*(inner(nabla_grad(psi), nabla_grad(phi)) + F*psi*phi) - beta*phi*psi.dx(0) )*dx
-L = Fwinds*phi*dx
+a = -r*inner(grad(psi), grad(phi))*dx - F*psi*phi*dx + beta*phi*psi.dx(0)*dx 
+L =  Fwinds*phi*dx
 
+#Question: Why does this not solve this correctly?
+
+"""
 # Set up Elliptic inverter
-psi_problem = LinearVariationalProblem(a,L,psi_soln, bcs=bc)
+psi_problem = LinearVariationalProblem(a, L, psi_soln, bcs=bc)
 psi_solver = LinearVariationalSolver(psi_problem,
                                      solver_parameters={
                                          'ksp_type':'cg',
                                          'pc_type':'sor'
                                       })
+"""
 
-psi0.project(psi_soln)
+solve(a == L, psi_soln, bcs=bc)
+
+#Question: Why does this not plot the solution?
+#plt.plot(psi_soln)
+#plt.show()
+
+# Potential Energy
 potential_energy = assemble(0.5*psi_soln*psi_soln*dx)
 print potential_energy
 
-outfile = File("outputpsi.pvd")
+# Output to a  file
+outfile = File("outputQG.pvd")
 outfile.write(psi_soln)
 
 
--- linear_stommel_sw.py
+++ linear_stommel_sw.py
@@ -1,5 +1,6 @@
 from firedrake import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Operators
 zcross   = lambda u: as_vector((-u[1],    u[0]))
@@ -7,7 +8,7 @@ gradperp = lambda u: as_vector((-u.dx(1), u.dx(0)))
 
 # Geometry
 Lx = 1.0
-Ly = 1.0#np.sqrt(2)
+Ly = 1.0
 n0 = 25
 mesh = RectangleMesh(n0, n0, Lx, Ly,  reorder=None) 
 
@@ -28,38 +29,49 @@ eta0 = Function(Vcg2, name='FreeSurface')
 sol = Function(Z)
 
 # Parameters and Functions
-r      = Constant("0.1")
+r      = Constant("0.2")
 beta   = Constant("1.0")
 tau    = Constant("0.001")
-F      = Constant("0.1")
+F      = Constant("1.0")
 fcor   = Function(Vcg2).interpolate(Expression("1.0+beta*x[1]", beta=beta))
 Fwinds = Function(Vcg2).interpolate(Expression("(Ly*tau/pi)*sin((pi*(x[1]-Ly/2.0))/Ly)", tau = tau, Ly = Ly))
 
 # Boundary Condtions
-# FJP: check that this imposes no-normal flow
+# Question 1: Does this impose u \cdot \hat h = 0?
 bc = DirichletBC(Z.sub(0), Constant((0, 0)), (1, 2, 3, 4))
+#bc  = [DirichletBC(Z.sub(0), Constant((1, 0)), (2,4)),
+#       DirichletBC(Z.sub(0), Constant((0, 1)), (1,3))]
 
 # Define the weak form
 a = r*inner(v,u)*dx - div(v)*eta*dx + lmbda*div(u)*dx + inner(fcor*v, zcross(u))*dx
 L = Fwinds*v[0]*dx
 
 # Set up SW inverter
+
+solve(a == L, sol, bcs=bc)
+# Question 2: Why does this solver not work?
+"""
 sw_problem = LinearVariationalProblem(a, L, sol, bcs=bc)
 sw_solver = LinearVariationalSolver(sw_problem,
                                      solver_parameters={
                                          'ksp_type':'cg',
                                          'pc_type':'sor'
                                       })
+"""
 
 # Split
 u, eta = sol.split()
 
-eta0.project(eta)
-potential_energy = assemble(0.5*eta0*eta0*dx)
-print potential_energy
+# Energies
+potential_energy = assemble(0.5*eta*eta*dx)
+kinetic_energy = assemble(0.5*u*u*dx)
 
-outfile = File("output.pvd")
-outfile.write(eta0)
+print kinetic_energy, potential_energy
+
+"""
+outfile = File("outputsw.pvd")
+outfile.write(eta)
 
 #eta_out = Function(Vcg, name='eta').assign(eta)
 #outfile.write(eta_out)
+"""
