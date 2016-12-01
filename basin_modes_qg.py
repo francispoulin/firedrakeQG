0ad42ade29a34e99c92a238e80b123a3b2771618
Francis J. Poulin <fpoulin@uwaterloo.ca>
2016-12-01 10:56:32 -0500

    Add files via upload

--- basin_modes_qg.py
+++ basin_modes_qg.py
@@ -30,29 +30,50 @@ m = -inner(grad(psi), grad(phi))*dx - F*psi*phi*dx
 petsc_a = assemble(a).M.handle
 petsc_m = assemble(m, bcs=bc).M.handle
 
-num_eigenvalues = 5                                 # Number of eigenvalues
+num_eigenvalues = 2                                 # Number of eigenvalues
 
-# Create solver
+opts = PETSc.Options()
+
+opts.setValue("eps_gen_non_hermitian", None)
+opts.setValue("st_pc_factor_shift_type", "NONZERO")
+opts.setValue("eps_type", "krylovschur")
+opts.setValue("eps_largest_imaginary", None)
+opts.setValue("eps_tol", 1e-10)
+
 es = SLEPc.EPS().create(comm=COMM_WORLD)
-es.setProblemType(es.ProblemType.GHEP)              # Generalized eigenvalue problem
-es.setWhichEigenpairs(es.Which.SMALLEST_IMAGINARY)  # type of eigenvalues to find
 es.setDimensions(num_eigenvalues)
-es.setOperators(petsc_a, petsc_m)                   # Build Operators
-es.solve()                                          # solve system
+es.setOperators(petsc_a, petsc_m)
+es.setFromOptions()
+es.solve()
 
 nconv = es.getConverged()
 print nconv
 
-# Find eigenvalues an eigenvectors
-eigenvalue = []
-eigefunction = []
-vr, vi = petsc_a.getVecs()
-for i in range(num_eigenvalues):
-    lam = es.getEigenpair(i, vr, vi)
-    print lam
-    #eigenvalue[i] = lam.real
-    #eigenfunction[i].vector()[:] = vr
+eigenmodes_real, eigenmodes_imag = Function(Vcg), Function(Vcg)
 
+# Find eigenvalues an eigenvectors
+eigenvaluer, eigenvaluei = [], []
+eigefunctionr, eigenfunctioni = [], []
+
+vr, vi = petsc_a.getVecs()
+for i in range(nconv):
+    lam = es.getEigenpair(i, vr, vi)
+    eigenvaluer.append(lam.real)
+    eigenvaluei.append(lam.imag)
+    #eigenfunction.vector()[:] = vr
+    vr_plot = vr
+    #p = plot(vr_plot)
+    #p.show()
+    
+eigenmodes_real.vector()[:], eigenmodes_imag.vector()[:] = vr, vi
+
+p = plot(eigenmodes_real)
+p.show()
+p = plot(eigenmodes_imag)
+p.show()
+
+#print eigenvaluer
+print eigenvaluei
 #print eigenvalue[0], eigenvalue[1]
 
 """"
